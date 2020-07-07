c56309ab576f913a6a05c80369ec3e9782ef6500
MajorDong <odsts@outlook.com>
2020-07-08 05:43:41 +0800

    install eslint-plugin-react-hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0)
+	return (
+		<div className="App">
+			<div>share-blog{count}</div>
+			<button
+				onClick={() => {
+					setCount(() => {
+						if (count % 2 === 0) {
+							return count + 3;
+						}
+						return count + 1;
+					}
+					)
+				}}
+			>
+				+1
+			 </button>
+		</div>
+	);
 }
 
 export default App;
